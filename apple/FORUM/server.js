95c2696179ad324d515b5a2f2b6d119d59c6d2f2
Min Kyung <01aldrn10@gmail.com>
2024-04-10 00:11:33 +0900

    s

--- apple/FORUM/server.js
+++ apple/FORUM/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
+app.set('view engine','ejs')
+app.use(express.static(__dirname+'/public'))
 
-app.listen(8080,()=>{ // port: 외부컴과 통신할 수 있다.  
-  console.log('http://localhost:8080 에서 서버 실행중');
-})
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:20131876@cluster0.ffx97gs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client)=>{ // 접속하고 성공하면
+  console.log('DB연결성공')
+  db = client.db('forum') // forum에 접속해줘
+  app.listen(8080,()=>{
+    console.log('http://localhost:8080 에서 서버 실행중');
+  })
+}).catch((err)=>{ // 근데 오류나면 오류출력
+  console.log(err)
+}) 
+
 app.get('/',(요청,응답)=>{
-  응답.sendFile(__dirname + '/index.html')
+  응답.sendFile(__dirname+'/index.html')
 })
+
 app.get('/news',(요청,응답)=>{
-  응답.send('오늘날씨가좋아')
+  // db.collection('post').insertOne({title:'어쩌구'})
+  // 응답.send('오늘비옴')
+})
+
+app.get('/list',async(요청,응답)=>{
+  let result = await db.collection('post').find().toArray()
+  응답.render('list.ejs',{ posts : result})
 })
